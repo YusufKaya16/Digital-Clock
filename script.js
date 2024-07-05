e9678c71def092779e58a5bfb0edb6364267bd82
Yusuf KAYA <yusuf.kaya1059@gmail.com>
2024-07-06 02:26:25 +0300

    refactor

--- script.js
+++ script.js
@@ -1,296 +1,183 @@
-const display = document.getElementById('display');
-const hour = display.querySelector('#hour');
-const minute = display.querySelector('#minute');
-const second = display.querySelector('#second');
-const modeSection = document.getElementById('mode-section');
-const mode = modeSection.querySelector('#mode');
-const startBtn = modeSection.querySelector('#start');
-const stopBtn = modeSection.querySelector('#stop');
-const resetBtn = modeSection.querySelector('#reset');
-const inputArea = modeSection.querySelector('#input-area');
+const hour = document.querySelector("#hour");
+const minute = document.querySelector("#minute");
+const second = document.querySelector("#second");
+const modeSelect = document.querySelector("#mode");
+const inputArea = document.querySelector("#input-area");
+const buttonGroup = document.querySelector("#button-group");
+const allButtons = [...buttonGroup.children];
+const hourInput = document.querySelector("#hour-input");
+const minuteInput = document.querySelector("#minute-input");
+const secondInput = document.querySelector("#second-input");
+const allInputs = [...document.querySelectorAll(".number-in")];
 
-const inputHour = modeSection.querySelector('#hour-input');
-const inputMinute = modeSection.querySelector('#minute-input');
-const inputSecond = modeSection.querySelector('#second-input');
 
-// setInterVal return values
-let clockID;
-let timerID;
-let countID;
+const clock = () => {
+  const time = new Date();
+  hour.innerHTML =
+    time.getHours() < 10 ? "0" + time.getHours() : time.getHours();
+  minute.innerHTML =
+    time.getMinutes() < 10 ? "0" + time.getMinutes() : time.getMinutes();
+  second.innerHTML =
+    time.getSeconds() < 10 ? "0" + time.getSeconds() : time.getSeconds();
+};
 
-let startBtnState = 0;
+const clearValues = () => {
+  hour.innerHTML = "00";
+  minute.innerHTML = "00";
+  second.innerHTML = "00";
+};
 
-allEvents();
-
-function allEvents() {
-
-    document.addEventListener('DOMContentLoaded', showClock);
-    mode.addEventListener('change', modeSelection);
-    startBtn.addEventListener('click', startOperation);
-    stopBtn.addEventListener('click', stopOperation);
-    resetBtn.addEventListener('click', resetOperation);
-    inputHour.addEventListener('change', inputHourReset);
-    inputMinute.addEventListener('change', inputMinuteReset);
-    inputSecond.addEventListener('change', inputSecondReset);
-
+const clearInputs = () => {
+    hourInput.value = "";
+    minuteInput.value = "";
+    secondInput.value = "";
 }
 
-function modeSelection(event) {
+let timing;
+let timerVar;
+let countdownVar;
+let x = 0;
 
-    const selectedMode = event.target.value;
-    startBtnState = 0;
+let tempHour = 0;
+let tempMin = 0;
+let tempSec = 0;
 
-    if (selectedMode === 'clock') {
-        mode.dataset.mode = selectedMode;
-        clearInterval(timerID);
-        clearInterval(countID);
-        startBtn.classList.remove('buttonChangeClass');
-        stopBtn.classList.remove('buttonChangeClass');
-        resetBtn.classList.remove('buttonChangeClass');
-        inputArea.classList.remove('inputChangeClass');
-        showClock();
+window.addEventListener("load", ()=>{
+    timing = setInterval(clock, 1000);
+});
+
+// Mode selecting
+modeSelect.addEventListener("change", (event) => {
+  const mode = event.target.value;
+  clearInputs();
+
+  if (mode === "clock") {
+    inputArea.classList.remove("inputChangeClass");
+    buttonGroup.classList.remove("buttonChangeClass");
+    timing = setInterval(clock, 1000);
+    clearInterval(timing);
+    clearInterval(countdownVar);
+
+  } else if (mode === "timer") {
+    buttonGroup.classList.add("buttonChangeClass");
+    inputArea.classList.remove("inputChangeClass");
+    clearValues();
+    clearInterval(timing);
+    clearInterval(countdownVar);
+
+  } else {
+    clearValues();
+    clearInterval(timing);
+    clearInterval(timerVar);
+    inputArea.classList.add("inputChangeClass");
+    buttonGroup.classList.add("buttonChangeClass");
+  }
+});
+
+const setValues = (hourVal, minuteVal, secondVal)=>{
+  hour.innerHTML = (hourVal < 10) && (hourVal !== "00") ? "0" + hourVal : hourVal;
+  minute.innerHTML = (minuteVal < 10) && (minuteVal !== "00") ? "0" + minuteVal : minuteVal;
+  second.innerHTML = (secondVal < 10) && (secondVal !== "00") ? "0" + secondVal : secondVal;
+}
+
+const getInnerHTML = ()=> ([hour.innerHTML, minute.innerHTML, second.innerHTML]);
+
+for (let button of allButtons) {
+  button.addEventListener("click", (event) => {
+    let buttonType = event.target.dataset.buttontype;
+
+    if(buttonType === "start"){
+      if(modeSelect.value === "countdown"){
+        setValues(tempHour, tempMin, tempSec);
+        countdownVar = setInterval(countdownStart, 1000);
+      }
+      else{
+        timerVar = setInterval(timerStart, 1000);
+      }
+    }
+    
+    else if(buttonType === "stop"){
+      if(modeSelect.value === "countdown"){
+        [tempHour, tempMin, tempSec] = getInnerHTML();
+        clearInterval(countdownVar);
+      }
+      else{
+        [hourCounter, minCounter, secCounter] = getInnerHTML();
+        clearInterval(timerVar);
+      }
+    }
+    
+    else{
+      if(modeSelect.value === "countdown"){
+        tempHour = hourInput.value;
+        tempMin = minuteInput.value;
+        tempSec = secondInput.value;
+        setValues(tempHour, tempMin, tempSec);
+      }
+      else{
+        secCounter = 0;
+        minCounter = 0;
+        hourCounter = 0;
+        clearValues();
+        clearInterval(timerVar);
+      }
     }
 
-    if (selectedMode === 'timer') {
-        mode.dataset.mode = selectedMode;
-        hour.textContent = 0;
-        minute.textContent = 0;
-        second.textContent = 0;
-        clearInterval(clockID);
-        clearInterval(countID);
-        startBtn.classList.add('buttonChangeClass');
-        stopBtn.classList.add('buttonChangeClass');
-        resetBtn.classList.add('buttonChangeClass');
-        inputArea.classList.remove('inputChangeClass');
+  });
+}
+
+for(let input of allInputs){
+  input.addEventListener('change', (event) =>{
+    if(event.target.id === "hour-input"){
+      tempHour = hourInput.value;
+    }
+    else if(event.target.id === "minute-input"){
+      tempMin = minuteInput.value;
+    }
+    else{
+      tempSec = secondInput.value;
+    }
+  })
+}
+
+const countdownStart = () => {
+    
+    if(hour.innerHTML === "00" && minute.innerHTML === "00" && second.innerHTML == "00"){
+        clearInterval(countdownVar);
     }
 
-    if (selectedMode === 'countdown') {
-        mode.dataset.mode = selectedMode;
-        hour.textContent = inputHour.value;
-        minute.textContent = inputMinute.value;
-        second.textContent = inputSecond.value;
-        clearInterval(clockID);
-        clearInterval(timerID);
-        inputArea.classList.add('inputChangeClass');
-        startBtn.classList.add('buttonChangeClass');
-        stopBtn.classList.add('buttonChangeClass');
-        resetBtn.classList.add('buttonChangeClass');
-    }
-}
+    else{
 
-function showClock() {
-
-    clockID = setInterval(() => {
-        const clock = new Date();
-        hour.textContent = clock.getHours();
-        minute.textContent = clock.getMinutes();
-        second.textContent = clock.getSeconds();
-    }, 1000);
-
-}
-
-function datasetVeri(hour, minute, second) {
-    mode.dataset.hour = hour;
-    mode.dataset.minute = minute;
-    mode.dataset.second = second;
-}
-
-function timerShow(saat, dakika, saniye) {
-
-    hour.textContent = saat;
-    minute.textContent = dakika;
-    second.textContent = saniye;
-
-    timerID = setInterval(() => {
-        second.textContent = parseInt(second.textContent) + 1;
-        if (second.textContent === '60') {
-            minute.textContent = parseInt(minute.textContent) + 1;
-            second.textContent = '0';
-            if (minute.textContent === '60') {
-                hour.textContent = parseInt(hour.textContent) + 1;
-                minute.textContent = '0';
-                if (hour.textContent === '24') {
-                    hour.textContent = '00';
-                    if (hour.textContent === '25') {
-                        hour.textContent = '1';
-                    }
-                }
+        if(second.innerHTML === "00"){
+            second.innerHTML = "60";
+            
+            if(minute.innerHTML === "00" &&hour.innerHTML !== "00"){
+                minute.innerHTML = "59";
+                hour.innerHTML = Number(hour.innerHTML) <= 10 ? "0" + (hour.innerHTML - 1) : hour.innerHTML - 1;
+            }
+            else{
+              minute.innerHTML = (Number(minute.innerHTML) <= 10) ? "0" + (minute.innerHTML - 1) : minute.innerHTML - 1;
             }
         }
-        datasetVeri(hour.textContent, minute.textContent, second.textContent);
-    }, 1000);
-
-}
-
-function timerReset() {
-    hour.textContent = 0;
-    minute.textContent = 0;
-    second.textContent = 0;
-    mode.dataset.hour = 0;
-    mode.dataset.minute = 0;
-    mode.dataset.second = 0;
-}
-
-function countdownInit(saat, dakika, saniye) {
-
-    hour.textContent = saat;
-    minute.textContent = dakika;
-    second.textContent = saniye;
-    datasetVeri(hour.textContent, minute.textContent, second.textContent);
-
-    // just second
-    if (hour.textContent === '0' && minute.textContent === '0' && second.textContent !== '0') {
-        countID = setInterval(() => {
-            second.textContent = parseInt(second.textContent) - 1;
-            if (second.textContent === '0') {
-                clearInterval(countID);
-            }
-            datasetVeri(hour.textContent, minute.textContent, second.textContent);
-        }, 1000);
+        second.innerHTML = Number(second.innerHTML) <= 10 ? "0" + (second.innerHTML - 1) : second.innerHTML - 1;
     }
+};
 
-    //just minute
-    else if (hour.textContent === '0' && minute.textContent !== '0' && second.textContent === '0') {
+let secCounter = 0;
+let minCounter = 0;
+let hourCounter = 0;
 
-        countID = setInterval(() => {
-            if (second.textContent === '0' && minute.textContent !== '0') {
-                minute.textContent = parseInt(minute.textContent) - 1;
-                second.textContent = '59';
-            }
-            else if (minute.textContent === '0' && second.textContent === '0') {
-                clearInterval(countID);
-            }
-            else {
-                second.textContent = parseInt(second.textContent) - 1;
-            }
-            datasetVeri(hour.textContent, minute.textContent, second.textContent);
-
-        }, 1000);
-
+const timerStart = ()=>{
+  ++secCounter;
+  second.innerHTML = Number(second.innerHTML) < 9 ? "0" + secCounter : secCounter;
+  if(second.innerHTML === "60"){
+    secCounter = 0;
+    ++minCounter;
+    minute.innerHTML = Number(minute.innerHTML) < 9 ? "0" + minCounter : minCounter;
+    if(minute.innerHTML === "60"){
+      minCounter = 0;
+      ++hourCounter;
+      hour.innerHTML = Number(hour.innerHTML) < 9 ? "0" + hourCounter : hourCounter;
+      }
     }
-
-    //just hour - start stop !!! hata
-    else if (hour.textContent !== '0' && minute.textContent === '0' && second.textContent === '0') {
-        countID = setInterval(() => {
-            if (hour.textContent !== '0' && minute.textContent === '0' && second.textContent === '0') {
-                hour.textContent = parseInt(hour.textContent) - 1;
-                minute.textContent = '59';
-                second.textContent = '59';
-            }
-            else if (hour.textContent !== '0' && minute.textContent !== '0' && second.textContent === '0') {
-                minute.textContent = parseInt(minute.textContent) - 1;
-                second.textContent = '59';
-            }
-            else if (hour.textContent === '0' && minute.textContent === '0' && second.textContent === '0') {
-                clearInterval(countID);
-            }
-            else {
-                second.textContent = parseInt(second.textContent) - 1;
-            }
-
-            datasetVeri(hour.textContent, minute.textContent, second.textContent);
-
-        }, 1000);
-    }
-
-    else {
-        countID = setInterval(() => {
-            if ((second.textContent === '0' && minute.textContent !== '0' && hour.textContent !== '0')) {
-                minute.textContent = parseInt(minute.textContent) - 1;
-                second.textContent = '59';
-            }
-            else if (second.textContent === '0' && minute.textContent === '0' && hour.textContent !== '0') {
-                hour.textContent = parseInt(hour.textContent) - 1;
-                minute.textContent = '59';
-                second.textContent = '59';
-            }
-            else if (second.textContent === '0' && minute.textContent === '0' && hour.textContent === '0') {
-                clearInterval(countID);
-            }
-            else {
-                second.textContent = parseInt(second.textContent) - 1;
-            }
-
-            datasetVeri(hour.textContent, minute.textContent, second.textContent);
-
-        }, 1000);
-    }
-}
-
-function startOperation(event) {
-
-    startBtnState += 1;
-
-    if (mode.dataset.mode === 'timer') {
-        if (startBtnState === 1) {
-            timerShow(0, 0, 0);
-        }
-        else {
-            timerShow(mode.dataset.hour, mode.dataset.minute, mode.dataset.second);
-        }
-    }
-
-    if (mode.dataset.mode === 'countdown') {
-        if (startBtnState === 1) {
-            if (inputHour.value === "" || inputMinute.value === "" || inputSecond.value === "") {
-                startBtnState = 0;
-                return alert('Enter values for the countdown!');
-            }
-            mode.dataset.hour = inputHour.value;
-            mode.dataset.minute = inputMinute.value;
-            mode.dataset.second = inputSecond.value;
-            countdownInit(mode.dataset.hour, mode.dataset.minute, mode.dataset.second);
-        }
-
-        else {
-            if (inputHour.value === "" || inputMinute.value === "" || inputSecond.value === "") {
-                startBtnState = 0;
-                return alert('Enter values for the countdown!');
-            }
-            countdownInit(mode.dataset.hour, mode.dataset.minute, mode.dataset.second);
-        }
-    }
-
-}
-function stopOperation(event) {
-
-    if (mode.dataset.mode === 'timer') {
-        clearInterval(timerID);
-    }
-
-    if (mode.dataset.mode === 'countdown') {
-        clearInterval(countID);
-    }
-
-}
-
-function resetOperation(event) {
-    if (mode.dataset.mode === 'timer') {
-        timerReset();
-    }
-
-    if (mode.dataset.mode === 'countdown') {
-        mode.dataset.hour = inputHour.value;
-        mode.dataset.minute = inputMinute.value;
-        mode.dataset.second = inputSecond.value;
-        hour.textContent = mode.dataset.hour;
-        minute.textContent = mode.dataset.minute;
-        second.textContent = mode.dataset.second;
-
-    }
-}
-
-function inputHourReset(event) {
-    hour.textContent = inputHour.value;
-    mode.dataset.hour = inputHour.value;
-}
-function inputMinuteReset(event) {
-    minute.textContent = inputMinute.value;
-    mode.dataset.minute = inputMinute.value;
-
-}
-function inputSecondReset(event) {
-    second.textContent = inputSecond.value;
-    mode.dataset.second = inputSecond.value;
-
 }
