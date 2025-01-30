f092043f116030f8f2abb14cb44cf37c6b930559
Yusuf KAYA <yusuf.kaya1059@gmail.com>
2025-01-31 01:10:31 +0300

    the codes have been revized

--- script.js
+++ script.js
@@ -1,182 +1,171 @@
-const hour = document.querySelector("#hour");
-const minute = document.querySelector("#minute");
-const second = document.querySelector("#second");
-const modeSelect = document.querySelector("#mode");
-const inputArea = document.querySelector("#input-area");
+const digits = document.querySelectorAll(".digit");
+const modeSelector = document.querySelector(".form-select");
+const buttons = document.querySelectorAll(".btn");
 const buttonGroup = document.querySelector("#button-group");
-const allButtons = [...buttonGroup.children];
+const inputGroup = document.querySelector("#input-area");
 const hourInput = document.querySelector("#hour-input");
 const minuteInput = document.querySelector("#minute-input");
 const secondInput = document.querySelector("#second-input");
-const allInputs = [...document.querySelectorAll(".number-in")];
 
-
-const clock = () => {
-  const time = new Date();
-  hour.innerHTML =
-    time.getHours() < 10 ? "0" + time.getHours() : time.getHours();
-  minute.innerHTML =
-    time.getMinutes() < 10 ? "0" + time.getMinutes() : time.getMinutes();
-  second.innerHTML =
-    time.getSeconds() < 10 ? "0" + time.getSeconds() : time.getSeconds();
-};
-
-const clearValues = () => {
-  hour.innerHTML = "00";
-  minute.innerHTML = "00";
-  second.innerHTML = "00";
-};
-
-const clearInputs = () => {
-    hourInput.value = "";
-    minuteInput.value = "";
-    secondInput.value = "";
-}
-
+// global variables
 let timing;
-let timerVar;
-let countdownVar;
-let x = 0;
 
-let tempHour = 0;
-let tempMin = 0;
-let tempSec = 0;
+// all events
+function addEvents() {
+  buttons.forEach((button) => {
+    button.addEventListener("click", handleButton);
+  });
 
-window.addEventListener("load", ()=>{
+  modeSelector.addEventListener("change", handleMode);
+
+  hourInput.addEventListener("input", handleInputChange);
+  minuteInput.addEventListener("input", handleInputChange);
+  secondInput.addEventListener("input", handleInputChange);
+
+  window.addEventListener("DOMContentLoaded", () => {
     timing = setInterval(clock, 1000);
-});
-
-// Mode selecting
-modeSelect.addEventListener("change", (event) => {
-  const mode = event.target.value;
-  clearInputs();
-
-  if (mode === "clock") {
-    inputArea.classList.remove("inputChangeClass");
-    buttonGroup.classList.remove("buttonChangeClass");
-    timing = setInterval(clock, 1000);
-    clearInterval(countdownVar);
-
-  } else if (mode === "timer") {
-    buttonGroup.classList.add("buttonChangeClass");
-    inputArea.classList.remove("inputChangeClass");
-    clearValues();
-    clearInterval(timing);
-    clearInterval(countdownVar);
-
-  } else {
-    clearValues();
-    clearInterval(timing);
-    clearInterval(timerVar);
-    inputArea.classList.add("inputChangeClass");
-    buttonGroup.classList.add("buttonChangeClass");
-  }
-});
-
-const setValues = (hourVal, minuteVal, secondVal)=>{
-  hour.innerHTML = (hourVal < 10) && (hourVal !== "00") ? "0" + hourVal : hourVal;
-  minute.innerHTML = (minuteVal < 10) && (minuteVal !== "00") ? "0" + minuteVal : minuteVal;
-  second.innerHTML = (secondVal < 10) && (secondVal !== "00") ? "0" + secondVal : secondVal;
-}
-
-const getInnerHTML = ()=> ([hour.innerHTML, minute.innerHTML, second.innerHTML]);
-
-for (let button of allButtons) {
-  button.addEventListener("click", (event) => {
-    let buttonType = event.target.dataset.buttontype;
-
-    if(buttonType === "start"){
-      if(modeSelect.value === "countdown"){
-        setValues(tempHour, tempMin, tempSec);
-        countdownVar = setInterval(countdownStart, 1000);
-      }
-      else{
-        timerVar = setInterval(timerStart, 1000);
-      }
-    }
-    
-    else if(buttonType === "stop"){
-      if(modeSelect.value === "countdown"){
-        [tempHour, tempMin, tempSec] = getInnerHTML();
-        clearInterval(countdownVar);
-      }
-      else{
-        [hourCounter, minCounter, secCounter] = getInnerHTML();
-        clearInterval(timerVar);
-      }
-    }
-    
-    else{
-      if(modeSelect.value === "countdown"){
-        tempHour = hourInput.value;
-        tempMin = minuteInput.value;
-        tempSec = secondInput.value;
-        setValues(tempHour, tempMin, tempSec);
-      }
-      else{
-        secCounter = 0;
-        minCounter = 0;
-        hourCounter = 0;
-        clearValues();
-        clearInterval(timerVar);
-      }
-    }
-
   });
 }
 
-for(let input of allInputs){
-  input.addEventListener('change', (event) =>{
-    if(event.target.id === "hour-input"){
-      tempHour = hourInput.value;
-    }
-    else if(event.target.id === "minute-input"){
-      tempMin = minuteInput.value;
-    }
-    else{
-      tempSec = secondInput.value;
-    }
-  })
-}
-
-const countdownStart = () => {
-    
-    if(hour.innerHTML === "00" && minute.innerHTML === "00" && second.innerHTML == "00"){
-        clearInterval(countdownVar);
-    }
-
-    else{
-
-        if(second.innerHTML === "00"){
-            second.innerHTML = "60";
-            
-            if(minute.innerHTML === "00" &&hour.innerHTML !== "00"){
-                minute.innerHTML = "59";
-                hour.innerHTML = Number(hour.innerHTML) <= 10 ? "0" + (hour.innerHTML - 1) : hour.innerHTML - 1;
-            }
-            else{
-              minute.innerHTML = (Number(minute.innerHTML) <= 10) ? "0" + (minute.innerHTML - 1) : minute.innerHTML - 1;
-            }
-        }
-        second.innerHTML = Number(second.innerHTML) <= 10 ? "0" + (second.innerHTML - 1) : second.innerHTML - 1;
-    }
+// handle input change event
+const handleInputChange = (event) => {
+  const input = event.target.name;
+  const value = event.target.value;
+  if (input === "hour") {
+    digits[0].textContent =
+      value < 9 ? String(value).padStart(2, "0") : String(value);
+  } else if (input === "minute") {
+    digits[1].textContent =
+      value < 9 ? String(value).padStart(2, "0") : String(value);
+  } else {
+    digits[2].textContent =
+      value < 9 ? String(value).padStart(2, "0") : String(value);
+  }
 };
 
-let secCounter = 0;
-let minCounter = 0;
-let hourCounter = 0;
+// handle event button
+const handleButton = (event) => {
+  const btnType = event.target.dataset.buttontype;
+  const mode = modeSelector.value;
 
-const timerStart = ()=>{
-  ++secCounter;
-  second.innerHTML = Number(second.innerHTML) < 9 ? "0" + secCounter : secCounter;
-  if(second.innerHTML === "60"){
-    secCounter = 0;
-    ++minCounter;
-    minute.innerHTML = Number(minute.innerHTML) < 9 ? "0" + minCounter : minCounter;
-    if(minute.innerHTML === "60"){
-      minCounter = 0;
-      ++hourCounter;
-      hour.innerHTML = Number(hour.innerHTML) < 9 ? "0" + hourCounter : hourCounter;
-      }
+  if (mode === "timer") {
+    if (btnType === "start") {
+      timing = setInterval(timerHandle, 1000);
+    } else if (btnType === "stop") {
+      clearInterval(timing);
+    } else {
+      clearInterval(timing);
+      resetDigits();
     }
-}
+  } else if (mode === "countdown") {
+    if (btnType === "start") {
+      countdownHandle();
+    } else if (btnType === "stop") {
+      clearInterval(timing);
+    } else {
+      clearInterval(timing);
+      resetDigits();
+      resetInput();
+    }
+  }
+};
+
+// handle event mode
+const handleMode = (event) => {
+  const mode = event.target.value;
+  resetDigits();
+  clearInterval(timing);
+
+  if (mode === "clock") {
+    buttonGroup.classList.remove("buttonsVisible");
+    inputGroup.classList.remove("inputsVisible");
+    timing = setInterval(clock, 1000);
+  } else if (mode === "timer") {
+    buttonGroup.classList.add("buttonsVisible");
+    inputGroup.classList.remove("inputsVisible");
+    clearInterval(timing);
+  } else {
+    buttonGroup.classList.add("buttonsVisible");
+    inputGroup.classList.add("inputsVisible");
+    clearInterval(timing);
+  }
+};
+
+const clock = () => {
+  const clk = new Date(Date.now());
+  setDisplay(clk.getHours(), clk.getMinutes(), clk.getSeconds());
+};
+
+const setDisplay = (hour, minute, second) => {
+  digits[0].textContent = hour < 9 ? `0${hour}` : hour;
+  digits[1].textContent = minute < 9 ? `0${minute}` : minute;
+  digits[2].textContent = second < 9 ? `0${second}` : second;
+};
+
+const resetDigits = () => {
+  digits.forEach((digit) => (digit.textContent = "00"));
+};
+
+const resetInput = () => {
+  hourInput.value = "";
+  minuteInput.value = "";
+  secondInput.value = "";
+};
+
+const timerHandle = () => {
+  digits[2].textContent =
+    Number(digits[2].textContent) < 9
+      ? String(Number(digits[2].textContent) + 1).padStart(2, "0")
+      : String(Number(digits[2].textContent) + 1);
+
+  if (Number(digits[2].textContent) > 59) {
+    digits[2].textContent = "00";
+    digits[1].textContent =
+      Number(digits[1].textContent) < 9
+        ? String(Number(digits[1].textContent) + 1).padStart(2, "0")
+        : String(Number(digits[1].textContent) + 1);
+  }
+
+  if (Number(digits[1].textContent) > 59) {
+    digits[1].textContent = "00";
+    digits[0].textContent =
+      Number(digits[0].textContent) < 9
+        ? String(Number(digits[0].textContent) + 1).padStart(2, "0")
+        : String(Number(digits[0].textContent) + 1);
+  }
+};
+
+const countdownHandle = () => {
+  clearInterval(timing);
+
+  let totalSeconds =
+    parseInt(hourInput.value) * 3600 +
+      parseInt(minuteInput.value) * 60 +
+      parseInt(secondInput.value) || 0;
+      
+  if (totalSeconds <= 0) {
+    alert("Lütfen saat, dakika ve saniye bilgisi girin!");
+    clearInterval(timing);
+    return;
+  }
+
+  function updateTimer() {
+
+    const hrs = String(Math.floor(totalSeconds / 3600));
+    const mins = String(Math.floor((totalSeconds % 3600) / 60));
+    const secs = String(totalSeconds % 60);
+
+    digits[0].textContent = hrs.padStart(2, "0");
+    digits[1].textContent = mins.padStart(2, "0");
+    digits[2].textContent = secs.padStart(2, "0");
+
+    if (totalSeconds <= 0) clearInterval(timing);
+    totalSeconds--;
+  }
+
+  updateTimer();
+  timing = setInterval(updateTimer, 1000);
+};
+
+// all events
+addEvents();
